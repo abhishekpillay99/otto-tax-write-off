3e97013c5f5e9a831d2a006324eb3574a2e89836
abhishekpillay99 <abhishek.pillay@gmail.com>
2025-04-23 23:25:12 +0530

    Create CameraIcon.tsx

---
+++ src/components/icons/CameraIcon.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+
+export const CameraIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#2b46f5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
+      <circle cx="12" cy="13" r="3" />
+    </svg>
+  );
+};
+
+export default CameraIcon;
