40bb5cfd55f625a16e1e71c603e5fcdc967aa9bf
abhishekpillay99 <abhishek.pillay@gmail.com>
2025-04-23 23:26:57 +0530

    Create ShieldIcon.tsx

---
+++ src/components/icons/ShieldIcon.tsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+export const ShieldIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#2b46f5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+    </svg>
+  );
+};
+
+export default ShieldIcon;
