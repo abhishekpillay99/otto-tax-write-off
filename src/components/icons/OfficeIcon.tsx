7990b12c9c3fb3fd595c2481c7a68cefa3901f1f
abhishekpillay99 <abhishek.pillay@gmail.com>
2025-04-23 23:26:41 +0530

    Create OfficeIcon.tsx

---
+++ src/components/icons/OfficeIcon.tsx
@@ -0,0 +1,26 @@
+
+import React from "react";
+
+export const OfficeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#2b46f5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="14" height="8" x="5" y="10" rx="1" />
+      <rect width="10" height="8" x="7" y="2" rx="1" />
+      <path d="M10 18v4" />
+      <path d="M14 18v4" />
+      <path d="M3 18h18" />
+    </svg>
+  );
+};
+
+export default OfficeIcon;
