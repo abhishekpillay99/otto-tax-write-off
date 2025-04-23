81836997ccaf03160ea010ad1c6559919b72e173
abhishekpillay99 <abhishek.pillay@gmail.com>
2025-04-23 23:26:26 +0530

    Create MegaphoneIcon.tsx

---
+++ src/components/icons/MegaphoneIcon.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+
+export const MegaphoneIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#2b46f5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m3 11 18-5v12L3 13v-2z" />
+      <path d="M11.6 16.8a3 3 0 1 1-5.8-1.6" />
+    </svg>
+  );
+};
+
+export default MegaphoneIcon;
