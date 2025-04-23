c3d415773d959fba873cb0bd1c09300a107b00e0
abhishekpillay99 <abhishek.pillay@gmail.com>
2025-04-23 23:25:52 +0530

    Create CarIcon.tsx

---
+++ src/components/icons/CarIcon.tsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+export const CarIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#2b46f5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14 16H9m10 0h3v-3.4a.6.6 0 0 0-.6-.6H3.6a.6.6 0 0 0-.6.6V16h3" />
+      <path d="M6 16v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1" />
+      <path d="M5 9l2-4h10l2 4" />
+    </svg>
+  );
+};
+
+export default CarIcon;
